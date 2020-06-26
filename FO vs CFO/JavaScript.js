6cb4d6a15c294b2520bd5a9d6dc5fa674cbd487d
manish106 <parmarmanishj2012@gmail.com>
2020-06-26 22:46:45 +0530

    Add function object vs constructor function object demo

---
+++ FO vs CFO/JavaScript.js
@@ -0,0 +1,19 @@
+﻿var app = angular.module('app', [])
+app.controller("demo", ["$scope", "service2", function ($scope, service2) {
+    console.log("Controller");
+    $scope.name = service2.xyz;
+
+}])
+app.service('service1', function () {
+    console.log("service1");
+    this.xyz = function () {
+        return "Manish";
+    };
+   
+});
+
+app.service('service2', function (service1) {
+    console.log("service2")
+    this.xyz = service1.xyz();
+   
+});
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Function-object-vs-Constructor-function-object
+
+This demo provide basic of function object vs constructor function object and javascript.js file provide Lazily instantiated in service
